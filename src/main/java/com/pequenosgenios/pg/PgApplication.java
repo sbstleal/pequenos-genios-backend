b26b2648734fee69ce41dc01485d35a2957bcdae
Sebastião Leal <sebastiaodeoleal@gmail.com>
2022-09-05 17:44:33 -0300

    First commit

---
+++ src/main/java/com/pequenosgenios/pg/PgApplication.java
@@ -0,0 +1,13 @@
+package com.pequenosgenios.pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pequenosgenios/pg/PgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pequenosgenios.pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
