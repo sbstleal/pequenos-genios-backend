cdf648669fd03de6f7d57e1d1717ec2bcd997ac8
DenisWm <denismamoni@hotmail.com>
2022-09-05 22:31:58 -0400

    Configurando ambiente de teste e desenvolvimento e criando classe de instanciaćão.

---
+++ src/main/java/com/pequenosgenios/pg/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.pequenosgenios.pg.config;
+
+import com.pequenosgenios.pg.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTesteDatabase();
+
+        return true;
+    }
+
+}
---
+++ src/main/java/com/pequenosgenios/pg/services/DBService.java
@@ -0,0 +1,11 @@
+package com.pequenosgenios.pg.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+
+    public void instantiateTesteDatabase() {
+        // instanciar os objetos e salvar no banco de dados
+    }
+}
