056c0ccc6a6ba56394f48c5374337e4d17ae8c84
Gilmar Moraes <gilmar14moraes@hotmail.com>
2022-09-06 22:23:30 -0300

    criação dos: controllers, dto, exception e service, alteração no repository- incompleto

---
+++ src/main/java/com/pequenosgenios/pg/exception/pGException.java
@@ -0,0 +1,14 @@
+package com.pequenosgenios.pg.exception;
+
+public class pGException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public pGException(String message) {
+		super(message);
+	}
+
+}
--- src/main/java/com/pequenosgenios/pg/repositories/StudentRepository.java
+++ src/main/java/com/pequenosgenios/pg/repositories/StudentRepository.java
@@ -4,8 +4,12 @@ import com.pequenosgenios.pg.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+	List<Student> findByEmail(String email);
 }
